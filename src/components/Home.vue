01e29164b8a999d3aeedfac5ed06331cdf494892
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-12-11 12:59:23 +0100

    Fix columns spacing in features

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -49,7 +49,7 @@
             <div class="col-md-4 col-md-offset-1 image">
                 <img src="../assets/placeholder.png" class="img-responsive center-block" />
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 col-md-offset-1">
                 <h2>auie</h2>
                 Nullam eu ante vel est convallis dignissim.  Fusce suscipit, wisi nec facilisis facilisis, est dui fermentum leo, quis tempor ligula erat quis odio.  Nunc porta vulputate tellus.  Nunc rutrum turpis sed pede.  Sed bibendum.  Aliquam posuere.  Nunc aliquet, augue nec adipiscing interdum, lacus tellus malesuada massa, quis varius mi purus non odio.  Pellentesque condimentum, magna ut suscipit hendrerit, ipsum augue ornare nulla, non luctus diam neque sit amet urna.  Curabitur vulputate vestibulum lorem.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed id ligula quis est convallis tempor.  Curabitur lacinia pulvinar nibh.  Nam a sapien.
             </div>
