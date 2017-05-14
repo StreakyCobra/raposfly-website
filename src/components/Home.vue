86847f2f4481e74046ca028303211f3ace12be72
Fabien Dubosson <fabien.dubosson@gmail.com>
2017-05-14 13:40:51 +0200

    Use translation file for project description

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -40,7 +40,7 @@
         <h1>{{ $t('home.project.title') }}</h1>
 
         <div class="row">
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.</p>
+            <p>{{ $t('home.project.description') }}</p>
         </div>
 
         <div class="sep"></div>
